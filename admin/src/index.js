ccd736fc16413c08d66d1f4358a140f4fd71e33d
akcyp <pycka.bartosz@gmail.com>
2021-02-26 08:10:04 +0100

    Fix

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,16 +1,9 @@
 import pluginPkg from '../../package.json';
 import pluginId from './pluginId';
-import App from './containers/App';
 import Initializer from './containers/Initializer';
 import lifecycles from './lifecycles';
 import trads from './translations';
 import PointList from './components/PointList/index';
-
-import messages from '../../../../.cache/admin/src/translations';
-Object.assign(messages.en, {
-  "content-type-builder.attribute.pointlist": "Point List"
-});
-
 
 export default strapi => {
   const pluginDescription = pluginPkg.strapi.description || pluginPkg.description;
