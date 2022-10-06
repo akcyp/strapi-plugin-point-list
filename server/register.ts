d20e7d89ff5d9e923314e6e93b0e06cdf6c3ac46
akcyp <pycka.bartosz@gmail.com>
2022-10-06 15:10:57 +0200

    Typescript config changes, remove package.json import (publish bug)

--- server/register.ts
+++ server/register.ts
@@ -1,11 +1,9 @@
 import { Strapi } from '@strapi/strapi';
-import pluginPkg from '../package.json';
 
 export default ({ strapi }: { strapi: Strapi }) => {
-
   strapi.customFields.register({
     name: 'point-list',
-    plugin: pluginPkg.strapi.name,
+    plugin: 'strapi-plugin-point-list',
     type: 'string',
   });
 };
